a23df2fce41356a8c76c79343193a5e16ef9a926
Kyler Eastridge <felix.wolfz@gmail.com>
2026-02-27 22:35:54 -0500

    Fix building

--- build-prod.sh
+++ build-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker build --progress=plain --network=host -t msoy-build . --no-cache
+docker build --progress=plain --network=host -t msoy-build .
 docker create --name extract msoy-build
 rm -r ./packages/*
 docker cp extract:/packages/ ./
